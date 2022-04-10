c6c20c4b95538568f2b5f3034e60f046ba80965b
BulatReznik <90419704+BulatReznik@users.noreply.github.com>
2022-04-10 21:23:21 +0400

    Альмухамметов Булат ПИБД-22 5 базовая

--- AutoRepairShopContacts/ViewModels/ClientViewModel.cs
+++ AutoRepairShopContacts/ViewModels/ClientViewModel.cs
@@ -1,20 +1,14 @@
 ﻿using System.ComponentModel;
-using System.Runtime.Serialization;
 
 namespace RepairContracts.ViewModels
 {
-    [DataContract]
     public class ClientViewModel
     {
-        [DataMember]
         public int Id { get; set; }
-        [DataMember]
         [DisplayName("ФИО клиента")]
         public string ClientFIO { get; set; }
-        [DataMember]
         [DisplayName("Логин")]
         public string Email { get; set; }
-        [DataMember]
         [DisplayName("Пароль")]
         public string Password { get; set; }
     }
